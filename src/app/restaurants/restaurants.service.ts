c45a7a44fde02d913b8941d81e078f707db89d04
dosv01 <danilosantos@outlook.com>
2017-09-01 19:00:14 -0300

    .

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
-
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
-
 import { MEAT_API } from '../app.api'
 import { Restaurant } from './restaurant/restaurant.model'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -30,5 +29,11 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
             .map(response => response.json())
             .catch(ErrorHandler.handlerError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
     }
 }
